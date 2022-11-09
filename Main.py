cc2cd562e6b912a186a4e43649818fb5159ebb9e
Cecilia Wang <ceciliawang@Cecilias-Mac.local>
2022-11-09 09:30:35 +0000

    refactor

--- Main.py
+++ Main.py
@@ -6,9 +6,7 @@ from pygame.locals import *
 from Board import *
 
 
-# 游戏屏幕的宽
 SCREEN_WIDTH = BLOCK_WIDTH * SIZE
-# 游戏屏幕的高
 SCREEN_HEIGHT = (BLOCK_HEIGHT + 2) * SIZE
 
 
@@ -33,7 +31,6 @@ def main():
     fwidth, fheight = font.size('999')
     font_color = (40, 40, 200)
 
-    # 加载资源图片，因为资源文件大小不一，所以做了统一的缩放处理
     img0 = pygame.image.load('resources/0.bmp').convert()
     img0 = pygame.transform.smoothscale(img0, (SIZE, SIZE))
     img1 = pygame.image.load('resources/1.bmp').convert()
@@ -86,7 +83,7 @@ def main():
         8: img8
     }
 
-    bgcolor = (225, 225, 225)   # 背景色
+    bg_color = (225, 225, 225)
 
     block = MineCell()
     game_status = GameStatus.readied
@@ -94,8 +91,7 @@ def main():
     elapsed_time = 0    # 耗时
 
     while True:
-        # 填充背景色
-        screen.fill(bgcolor)
+        screen.fill(bg_color)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -106,15 +102,12 @@ def main():
                 y = mouse_y // SIZE - 2
                 b1, b2, b3 = pygame.mouse.get_pressed()
                 if game_status == GameStatus.started:
-                    # 鼠标左右键同时按下，如果已经标记了所有雷，则打开周围一圈
-                    # 如果还未标记完所有雷，则有一个周围一圈被同时按下的效果
                     if b1 and b3:
                         mine = block.get_mine(x, y)
                         if mine.status == CellStatus.clicked:
                             if not block.double_mouse_button_down(x, y):
                                 game_status = GameStatus.over
             elif event.type == MOUSEBUTTONUP:
-                # if y < 0:
                 if face_pos_x <= mouse_x <= face_pos_x + face_size \
                         and face_pos_y <= mouse_y <= face_pos_y + face_size:
                     game_status = GameStatus.readied
@@ -177,7 +170,8 @@ def main():
         print_text(screen, font, 30, (SIZE * 2 - fheight) // 2 - 2, '%02d' % (MINE_COUNT - flag_count), font_color)
         if game_status == GameStatus.started:
             elapsed_time = int(time.time() - start_time)
-        print_text(screen, font, SCREEN_WIDTH - fwidth - 30, (SIZE * 2 - fheight) // 2 - 2, '%03d' % elapsed_time, font_color)
+        print_text(screen, font, SCREEN_WIDTH - fwidth - 30,
+                   (SIZE * 2 - fheight) // 2 - 2, '%03d' % elapsed_time, font_color)
 
         if flag_count + opened_count == BLOCK_WIDTH * BLOCK_HEIGHT:
             game_status = GameStatus.win
